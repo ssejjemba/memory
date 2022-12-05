a6db47711eacda8c68eb4d015705b81cf4d447d1
arthurkyambadde <arthurkyambadde9@gmail.com>
2022-12-05 10:49:57 +0300

    feat(Icons): add icons to the application

--- Game/memorygame/pages/index.tsx
+++ Game/memorygame/pages/index.tsx
@@ -1,5 +1,14 @@
-import styles from '../styles/Home.module.css';
+import { AnchorIcon } from '../components/Icons/anchor-icon/AnchorIcon';
+
+import styles from './styles.module.css';
 
 export default function Home() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      Hello World
+      <div>
+        <AnchorIcon className={styles.icon} />
+      </div>
+    </div>
+  );
 }
