5a83d105add8946c2f7bd014058c6d3ca1908b5b
DESKTOP-G60BJ3E\Ssonko Jimmy <ssembatyadennis25@gmail.com>
2022-12-06 03:16:42 +0300

    test(Buttons): try to add tests for the primary button.
    - endevoured to add tests for the button component but the testing library/react failed to install.

---
+++ src/components/buttons/__test__/ButtonPrimary.test.tsx
@@ -0,0 +1,10 @@
+import { ButtonPrimary } from "../button-primary/ButtonPrimary";
+import "@testing-library/jest-dom";
+// import { render } from "@testing-library/react";
+import test, { describe } from "node:test";
+
+describe("test cases for the ButtonPrimary Component in the UI", () => {
+  test("test weather the ButtonPrimary has been successfully rendered in the document", () => {
+    // Falied to use react testing library
+  });
+});
